fc66ad581deb9b7a45360b513716601350af3dca
Vishal Hulawale <hulawale.vishal@gmail.com>
2022-07-09 13:17:48 +0530

    #2 Created Spring boot app

---
+++ src/main/java/com/virtman/mgmtengine/MgmtengineApplication.java
@@ -0,0 +1,13 @@
+package com.virtman.mgmtengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgmtengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgmtengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtman/mgmtengine/MgmtengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtman.mgmtengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgmtengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
